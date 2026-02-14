1d5c9d3dc73a55a2a7382d6bc86e4110e98151f0
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-02-14 21:08:07 +0200

    add dotenv for environment variable management and update server port configuration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import "dotenv/config";
 import app from "./app"
 
-app.listen(8080, () => {
-    console.log("Listening on port 8080");
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
