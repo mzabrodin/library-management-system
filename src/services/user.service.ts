b867fecd20c591486dfa2ff8a951b542d69464d5
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-02-15 10:07:40 +0200

    implement UserService

---
+++ src/services/user.service.ts
@@ -0,0 +1,36 @@
+import {db} from "../storage/db";
+import {User} from "../types";
+import {UserDto} from "../schemas/userSchema";
+
+export class UserService {
+
+    findAll(): User[] {
+        return db.users.getAll();
+    }
+
+    findById(id: string): User | undefined {
+        return db.users.getById(id);
+    }
+
+    existsByEmail(email: string): boolean {
+        return db.users.getAll().some(user => user.email === email);
+    }
+
+    existsById(id: string): boolean {
+        return db.users.getById(id) !== undefined;
+    }
+
+    create(dto: UserDto): User {
+        const id = crypto.randomUUID();
+        const user: User = {
+            id,
+            name: dto.name,
+            email: dto.email
+        }
+
+        db.users.saveToMap(user);
+        db.users.saveToFile()
+
+        return user;
+    }
+}
