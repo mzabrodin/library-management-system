8c485b08332d74e165116148f967b23f9a32c403
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-02-15 15:33:01 +0200

    implement LoanService

---
+++ src/services/loan.service.ts
@@ -0,0 +1,43 @@
+import {db} from "../storage/db";
+import {Book, Loan, User} from "../types";
+import {LoanDto} from "../schemas/loan.schema";
+
+export class LoanService {
+    findAll(): Loan[] {
+        return db.loans.getAll();
+    }
+
+    existsActiveLoanForBook(bookId: string): boolean {
+        return this.findAll().some(loan => loan.bookId === bookId && loan.status === "ACTIVE");
+    }
+
+    loan(dto: LoanDto, user: User, book: Book): Loan {
+        const id = crypto.randomUUID();
+        const loan: Loan = {
+            id,
+            userId: user.id,
+            bookId: book.id,
+            loanDate: dto.loanDate || new Date(),
+            returnDate: dto.returnDate || null,
+            status: dto.status || "ACTIVE"
+        }
+
+        db.loans.saveToMap(loan);
+        db.loans.saveToFile();
+
+        return loan;
+    }
+
+    return(dto: LoanDto, loan: Loan): Loan {
+        const updatedLoan: Loan = {
+            ...loan,
+            returnDate: dto.returnDate || new Date(),
+            status: dto.status || "RETURNED"
+        }
+
+        db.loans.saveToMap(updatedLoan);
+        db.loans.saveToFile();
+
+        return updatedLoan;
+    }
+}
