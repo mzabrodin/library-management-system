7a09be02ab1b8707cf271690e6b1f73e20236ce6
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-02-15 16:41:53 +0200

    set default port to 8080 in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app"
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
