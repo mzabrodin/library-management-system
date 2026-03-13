49beae156c15099216f1361f326db49f912c498c
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-03-13 22:15:23 +0200

    add middleware for API error handling and not found responses

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import routes from "./routes";
+import {notFound} from "./middleware/notFound.middleware";
+import {errorHandler} from "./middleware/errorHandler.middleware";
 
 const app = express();
 
 app.use(express.json());
-app.use(routes)
+app.use("/api", routes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
