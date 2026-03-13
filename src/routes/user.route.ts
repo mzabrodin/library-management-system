9c6b82c99d75b2c7ded66fa558821be8d35e5cd1
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-03-14 01:14:02 +0200

    fix route to get current user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import {validate} from "../middleware/validate.middleware";
-import {registerSchema} from "../schemas/user.schema";
 import * as UserController from "../controllers/user.controller";
 import {authenticateJWT, requireAdmin} from "../middleware/auth.middleware";
 
@@ -8,8 +6,8 @@ const router = express.Router();
 
 router.get("/", authenticateJWT, requireAdmin, UserController.getAllUsers);
 
-router.get("/:id", authenticateJWT, requireAdmin, UserController.getUserById);
+router.get("/me", authenticateJWT, UserController.getCurrentUser);
 
-router.get("/me", authenticateJWT, validate(registerSchema), UserController.getCurrentUser);
+router.get("/:id", authenticateJWT, requireAdmin, UserController.getUserById);
 
 export default router;
