0e845f2ccc61739a01efa334aa4551a029001c78
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-03-14 01:13:38 +0200

    make userId optional in LoanSchema for flexibility

--- src/schemas/loan.schema.ts
+++ src/schemas/loan.schema.ts
@@ -4,7 +4,7 @@ export const loanStatusSchema = z
     .enum(["ACTIVE", "RETURNED"], {error: "Status must be either 'ACTIVE' or 'RETURNED'"});
 
 export const LoanSchema = z.object({
-    userId: z.uuid({error: "User ID must be a valid UUID"}),
+    userId: z.uuid({error: "User ID must be a valid UUID"}).optional(),
 
     bookId: z.uuid({error: "Book ID must be a valid UUID"}),
 
