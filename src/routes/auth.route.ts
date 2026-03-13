0adf6d736370f361086e329bf147389aa01770dc
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-03-14 01:54:55 +0200

    add JWT authentication middleware to refresh token route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {validate} from "../middleware/validate.middleware";
 import {loginSchema, registerSchema} from "../schemas/user.schema";
 import * as AuthController from "../controllers/auth.controller";
+import {authenticateJWT} from "../middleware/auth.middleware";
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ router.post("/register", validate(registerSchema), AuthController.register);
 
 router.post("/login", validate(loginSchema), AuthController.login);
 
-router.post("/refresh", AuthController.refresh);
+router.post("/refresh", authenticateJWT, AuthController.refresh);
 
 export default router;
