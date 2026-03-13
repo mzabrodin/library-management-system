02339db37eed35af1610c4dc850720f1fa5e4c70
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-03-14 00:22:57 +0200

    add authentication routes for user registration and login

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {validate} from "../middleware/validate.middleware";
+import {loginSchema, registerSchema} from "../schemas/user.schema";
+import * as AuthController from "../controllers/auth.controller";
+
+const router = express.Router();
+
+router.post("/register", validate(registerSchema), AuthController.register);
+
+router.post("/", validate(loginSchema), AuthController.login);
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import userRoutes from "./user.route";
 import bookRoute from "./book.route";
 import loanRoute from "./loan.route";
+import authRoute from "./auth.route";
 
 const router = express.Router();
 
 router.use("/books", bookRoute)
 router.use("/users", userRoutes);
 router.use("/loans", loanRoute);
+router.use("/auth", authRoute);
 
 export default router;
